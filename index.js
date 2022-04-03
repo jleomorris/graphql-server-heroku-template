94bc7e6991d9b26f59ee665987bf1dbe13147ac5
James Morris <70540466+jleomorris@users.noreply.github.com>
2022-04-03 02:00:14 +0100

    Enable cors

--- index.js
+++ index.js
@@ -41,7 +41,12 @@ const books = [
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
+const server = new ApolloServer({
+  cors: true,
+  typeDefs,
+  resolvers,
+  introspection: true,
+});
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
